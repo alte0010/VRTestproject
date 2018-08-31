d31e5b394241c6409b0bb50352d9d877a86fece7
Dyon Altena <Alte0010@hz.nl>
2018-08-31 11:47:29 +0200

    Game basics, cube physics and basic design

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public Playermovement movement;
+
+    // Use this for initialization
+    void OnCollisionEnter (Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Öbstacle")
+        {
+            movement.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
